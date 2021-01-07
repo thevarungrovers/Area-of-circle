0a82c55599eca924dfcb42ec70bb1232da8dfd6c
Varun Grover <grover82vinni@gmail.com>
2021-01-07 21:24:12 +0530

    file added.

---
+++ Area Of Circle.py
@@ -0,0 +1,7 @@
+r = input("Input the radius of the circle:") # input from user
+
+r = float(r) # typecasting
+
+a = 3.14159265 * (r**2) # calculating area
+
+print(f'The area of circle with radius {r} is {a}') # return area
